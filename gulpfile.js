0ed269bfdfc49549696ad06358e9de11e99289fa
Jessica Karpovich <karpovichjm@gmail.com>
2018-01-24 12:34:20 -0800

    Installed gulp-postcss and autoprefixer.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+/* Requires */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// postcss required for autoprefixer
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+
+/* Tasks */
+gulp.task('scss', function() {
+    
+    // Add support to last two versions of modern browsers
+    var processors = [
+        autoprefixer({ browsers: ['last 2 versions']}),
+    ];
+    
+    return gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./css'))
+        
+        // Make sure Browsersync runs after the css is finished compiling
+        .pipe(browserSync.reload({
+            stream:true
+    }))
+})
+
+// Initiallize Browsersync
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+})
+
+
+gulp.task('watch', ['browser-sync', 'scss'], function() {
+    // glob watches for files and files inside folders
+    gulp.watch('./scss/**/*.scss', ['scss'])
+})
