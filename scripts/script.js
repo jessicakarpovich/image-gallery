ab7586a85dce95bd4fd8edb9c060cf03e57173f6
Jessica Karpovich <karpovichjm@gmail.com>
2018-01-25 18:51:23 -0800

    Adjusted search url to produce more consistent design.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
 const appID = "359487dd6dc4bc12070cc4fa97a7ad7720afc3586bed232a673fde6ad5725608";
-const secret = "e41d209d1dcc7c7e84abc96a4449f102f3efbb4cc4c86253e0a9b40875208b6c";
 const api_endpoint = "https://api.unsplash.com/search/photos/?";
 const query = "clouds";
 // orientation is optional and can be landscape, portrait, or squarish
-const squareShape = "&orientation=sqaurish";
+const orientation = "&orientation=landscape";
 // per_page is optional, default 10
-// const limit9 = "&per_page=9";
+const limit = "&per_page=12";
 
-const url = api_endpoint + "client_id=" + appID + "&query=" + query;
+const url = api_endpoint + "client_id=" + appID + "&query=" + query + limit + orientation;
 
 
 /***** Load images by using fetch *****/
@@ -33,24 +32,22 @@ function showContent(data) {
     
     /* make sure that i does not exceed the number of results per page
         check the per_page value in the url before adjusting for loop */
-    for (let i=0; i < 9; i++) {
+    for (let i=0; i < 12; i++) {
         content += "<article>";
         
         content += "<img src='" + data.results[i].urls.small + "'";
         content += "srcset='" + data.results[i].urls.small + " 400w, ";
         content += data.results[i].urls.regular + " 1080w, ";
         content += data.results[i].urls.full + " 3504w'";
-        /*
-        content += "sizes='(max-width: 600px) 100vw, ";
-        content += "(max-width: 800px) 46vw, ";
-        content += "25vw'";
-        */
+        
         content += " alt='" + query + " photo by " + data.results[i].user.name + "'>";
+        
         // include link back to photographer, follow attribution guideline
         content += "<div class=photo-info>";
         content += "<p class='author'><a href='" + data.results[i].user.links.html + 
             "?utm_source=image_gallery&utm_medium=referral'>" + data.results[i].user.name + "</a></p>";
-        content += "<p class='likes'>" + data.results[i].likes + "</p></div>";
+        content += "<p class='likes'>" + data.results[i].likes + "</p>";
+        content += "</div>";
         content += "</article>";
     }
     
